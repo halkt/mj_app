949b8f67a85ee47134efc618a6eae52dda99423c
halmuda <halmuda6231@gmail.com>
2019-02-10 18:42:32 +0900

    User Create finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    redirect_to users_url, notice: "ユーザー「#{user.name}」を更新しました。"
   end
 
   def create
     user = User.new(user_params)
     user.save!
     redirect_to users_url, notice: "ユーザー「#{user.name}」を登録しました。"
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_url, notice: "ユーザー「#{user.name}」を削除しました。"
   end
 
   private
