9804f14cbcac6cba914cadff8e0e119db0e3681a
halmuda <halmuda6231@gmail.com>
2019-03-13 00:06:42 +0900

    Delete records dbfile

--- db/migrate/20190216151915_add_column_records.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnRecords < ActiveRecord::Migration[5.2]
-  def change
-    add_column :records, :genten, :float
-    add_column :records, :kaeshiten, :float
-    add_column :records, :uma, :string
-    add_column :records, :yakitori, :string
-  end
-end
