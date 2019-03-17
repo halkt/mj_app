f061085ef7eadc34bd81438df955cb0c68d3e04f
halmuda <halmuda6231@gmail.com>
2019-03-18 00:17:56 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,62 @@
+# adminユーザーを作成する
 User.create!(name:  "admin",
              mail: "admin@example.com",
              admin: true,
              password:              "password",
              password_confirmation: "password")
 
+# ウマを作成する
+horse.create(name: "無し", point1: 0, point2: 0)
+horse.create(name: "5-10", point1: 5000, point2: 10000)
+horse.create(name: "10-20", point1: 10000, point2: 20000)
+
 5.times do |i|
   i += 1
+  # ユーザーを作成する
   User.create(
     name: "テストユーザー#{i}",
-    mail: "user#{i}@example.com"
+    mail: "user#{i}@example.com",
+    admin: false,
+    password:              "password",
+    password_confirmation: "password"
   )
 
+  # イベントを作成する
   Event.create(
     name: "テスト大会#{i}"
   )
+  # ゲームを作成する
+  Game.create(event_id: 1,
+              genten: 25000,
+              kaeshiten: 30000,
+              horse_id: 1
+              )
+  # ゲーム詳細
+  GameDetail.create(game_id: i,
+                    user_id: 1,
+                    point: 40000,
+                    rank: 1
+                    )
+  GameDetail.create(game_id: i,
+                    user_id: i,
+                    point: 30000,
+                    rank: 2
+                    )
+  GameDetail.create(game_id: i,
+                    user_id: 3,
+                    point: 20000,
+                    rank: 3
+                    )
+  GameDetail.create(game_id: i,
+                    user_id: 4,
+                    point: 10000,
+                    rank: 4
+                    )
 end
 
-EventUser.create(user_id: 1, event_id: 1)
-EventUser.create(user_id: 1, event_id: 2)
-EventUser.create(user_id: 1, event_id: 3)
+# イベントにユーザーを紐付ける
 EventUser.create(user_id: 2, event_id: 1)
-EventUser.create(user_id: 2, event_id: 2)
+EventUser.create(user_id: 3, event_id: 1)
+EventUser.create(user_id: 4, event_id: 1)
+EventUser.create(user_id: 5, event_id: 1)
+EventUser.create(user_id: 6, event_id: 1)
