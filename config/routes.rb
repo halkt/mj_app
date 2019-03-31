af02cafa9e478ee8cb4ed61cc246407255c9f134
halmuda <halmuda6231@gmail.com>
2019-04-01 00:41:00 +0900

    seed update

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
   get 'mypages/show'
+  root to: 'mypages#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  #get 'static_pages/home'
   get 'static_pages/help'
   namespace :admin do
     resources :users
     resources :horses, only:[ :index, :create, :new, :destroy, :edit, :update]
   end
-  #resources :users
-  root to: 'mypages#show'
   resources :events do
       delete :event_users, to: 'event_users#destroy_all'
-      #resources :event_users, only:[ :index ,:create, :new ]
       resources :games, only:[ :create, :new, :edit, :update, :show, :destroy ]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
-# adminユーザーを作成する
-User.create!(name:  "admin",
-             mail: "admin@example.com",
+# ユーザーを作成する
+User.create!(name:  "サンプルくん",
+             mail: "sample@example.com",
              admin: true,
-             password:              "password",
-             password_confirmation: "password")
+             password:              "simple@sample",
+             password_confirmation: "simple@sample")
 
-# ウマを作成する
-Horse.create(name: "無し", point1: 0, point2: 0)
-Horse.create(name: "5-10", point1: 5000, point2: 10000)
-Horse.create(name: "10-20", point1: 10000, point2: 20000)
-
-5.times do |i|
+19.times do |i|
   i += 1
   # ユーザーを作成する
   User.create(
@@ -20,47 +15,58 @@ Horse.create(name: "10-20", point1: 10000, point2: 20000)
     password:              "password",
     password_confirmation: "password"
   )
-
-  # イベントを作成する
-  Event.create(
-    name: "テスト大会#{i}"
-  )
-  # ゲームを作成する
-  Game.create(event_id: 1,
-              genten: 25000,
-              kaeshiten: 30000,
-              horse_id: 1
-              )
-
 end
 
-# イベントにユーザーを紐付ける
-EventUser.create(user_id: 2, event_id: 1)
-EventUser.create(user_id: 3, event_id: 1)
-EventUser.create(user_id: 4, event_id: 1)
-EventUser.create(user_id: 5, event_id: 1)
-EventUser.create(user_id: 6, event_id: 1)
+# ウマを作成する
+Horse.create(name: "無し", point1: 0, point2: 0)
+Horse.create(name: "5-10", point1: 5000, point2: 10000)
+Horse.create(name: "10-20", point1: 10000, point2: 20000)
 
-5.times do |j|
-  # ゲーム詳細
-  GameDetail.create(game_id: j,
+# イベントを作成する
+5.times do |i|
+  i += 1
+  Event.create(
+    name: "麻雀大会#{i}"
+    day: "2019-04-01"
+  )
+  # イベント
+  EventUser.create(user_id: i, event_id: 1)
+  EventUser.create(user_id: i, event_id: 2)
+  EventUser.create(user_id: i, event_id: 3)
+  EventUser.create(user_id: i, event_id: 4)
+  EventUser.create(user_id: i, event_id: 5)
+end
+
+# ゲームを作成する
+10.times do |i|
+  i += 1
+  5.times do |j|
+    j += 1
+    Game.create(event_id: j,
+                genten: 25000,
+                kaeshiten: 30000,
+                horse_id: 1
+                )
+  end
+end
+
+# ゲーム詳細
+5.times do |i|
+  i += 1
+  GameDetail.create(game_id: i,
+                    user_id: 1,
+                    point: 40000
+                    )
+  GameDetail.create(game_id: i,
                     user_id: 2,
-                    point: 40000,
-                    rank: 1
+                    point: 30000
                     )
-  GameDetail.create(game_id: j,
+  GameDetail.create(game_id: i,
                     user_id: 3,
-                    point: 30000,
-                    rank: 2
+                    point: 20000
                     )
-  GameDetail.create(game_id: j,
+  GameDetail.create(game_id: i,
                     user_id: 4,
-                    point: 20000,
-                    rank: 3
-                    )
-  GameDetail.create(game_id: j,
-                    user_id: 5,
-                    point: 10000,
-                    rank: 4
+                    point: 10000
                     )
 end
