959403c82ea73e20fcedd086c64eb4f5eaa5869b
halmuda <halmuda6231@gmail.com>
2019-04-07 22:48:28 +0900

    Small renovation

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,8 +1,5 @@
 class RankingsController < ApplicationController
   def show
-    # すべての起点
     @gamedetails = GameDetail.limit(10).order('sum_score desc').group(:user_id).sum(:score)
-
-
   end
 end
