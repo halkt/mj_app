c7ff4fceb7825f2fe5bda1336ca1f105799dd5e1
“halmuda” <halmuda6231@gmail.com>
2020-08-08 17:32:19 +0900

    update: seedファイルのリファクタリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # ユーザーを作成する
-User.create!(name:  "サンプルくん",
-             mail: "sample@example.com",
+User.create!(name: 'サンプルくん',
+             mail: 'sample@example.com',
              admin: true,
              login_flg: true,
-             password:              "simple@sample",
-             password_confirmation: "simple@sample")
+             password: 'simple@sample',
+             password_confirmation: 'simple@sample')
 
 19.times do |i|
   i += 1
@@ -14,27 +16,25 @@ User.create!(name:  "サンプルくん",
     mail: "user#{i}@example.com",
     admin: false,
     login_flg: false,
-    password:              "password",
-    password_confirmation: "password"
+    password: 'password',
+    password_confirmation: 'password'
   )
 end
 
-# ウマを作成する
-Horse.create(name: "無し", point1: 0, point2: 0)
-Horse.create(name: "5-10", point1: 5000, point2: 10000)
-Horse.create(name: "10-20", point1: 10000, point2: 20000)
+Horse.create(name: '無し', point1: 0, point2: 0)
+Horse.create(name: '5-10', point1: 5000, point2: 10_000)
+Horse.create(name: '10-20', point1: 10_000, point2: 20_000)
 
-# コミュニティを作成する
-Community.create(name: "麻雀部")
-Community.create(name: "N会")
-Community.create(name: "テスト")
+Community.create(name: '麻雀部')
+Community.create(name: 'N会')
+Community.create(name: 'テスト')
 
 # イベントを作成する
 5.times do |i|
   i += 1
   Event.create(
     name: "麻雀大会#{i}",
-    day: "2019-04-01"
+    day: '2019-04-01'
   )
   # イベント
   EventUser.create(user_id: i, event_id: 1)
@@ -48,11 +48,9 @@ end
 5.times do |i|
   i += 1
   Game.create(event_id: i,
-              genten: 25000,
-              kaeshiten: 30000,
-              horse_id: 1
-              )
-
+              genten: 25_000,
+              kaeshiten: 30_000,
+              horse_id: 1)
 end
 
 # ゲーム詳細
@@ -60,26 +58,22 @@ end
   i += 1
   GameDetail.create(game_id: i,
                     user_id: 1,
-                    point: 40000,
+                    point: 40_000,
                     rank: 1,
-                    score: 30.0
-                    )
+                    score: 30.0)
   GameDetail.create(game_id: i,
                     user_id: 2,
-                    point: 30000,
+                    point: 30_000,
                     rank: 2,
-                    score: 0.0
-                    )
+                    score: 0.0)
   GameDetail.create(game_id: i,
                     user_id: 3,
-                    point: 20000,
+                    point: 20_000,
                     rank: 3,
-                    score: -10.0
-                    )
+                    score: -10.0)
   GameDetail.create(game_id: i,
                     user_id: 4,
-                    point: 10000,
+                    point: 10_000,
                     rank: 4,
-                    score: -20.0
-                    )
+                    score: -20.0)
 end
