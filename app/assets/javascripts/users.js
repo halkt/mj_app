8383b11f554c62fd37e26f39f0c7a634a033958c
“halmuda” <halmuda6231@gmail.com>
2020-09-13 18:17:46 +0900

    update: #33 ユーザ登録処理の不備を修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,18 @@
+// TODO: 最終的に削除する予定
 // ログインする場合のみ入力フォームを表示する
-var displayInputForm = () => {
-    flg = document.getElementById('user_login_flg').checked
-    target = document.getElementById('login-only'); 
-    if (flg == true) {
-        target.style.display='block';
-    } else {
-        target.style.display='none';
-    }
+const displayInputForm = () => {
+  flg = document.getElementById('user_login_flg').checked
+  target = document.getElementById('login-only'); 
+  if (flg == true) {
+      target.style.display='block';
+  } else {
+      target.style.display='none';
+  }
 } ;
 
-var profileIconSet = (index) => {
-    img = '/assets/images/profile-icon/' + index + '_profile_icon.png';
-    document.getElementById('users-icon').src = img;
+// 画像を出し分ける処理のみ追加
+const setProfileIcon = index => {
+  const images = document.querySelectorAll('.pic_frame > img');
+  const target = document.querySelector('.profile-icon > img');
+  target.src = images[index].src;
 }
