d67c3d667a44a39d95e28ab429194f275a2a3380
“halmuda” <halmuda6231@gmail.com>
2020-09-25 18:33:00 +0900

    add: #54 eventsのGET系テストの追加

--- spec/request/events_spec.rb
+++ spec/request/events_spec.rb
@@ -3,15 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :request do
-  # before do
-  #   user = FactoryBot.create(:user)
-  #   sign_in user
-  # end
+  let!(:event) { FactoryBot.create(:event) }
 
-  # describe 'GET /events' do
-  #   it 'ログイン画面の表示に成功すること' do
-  #     get events_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+  describe 'GET /events', :login do
+    it 'ログイン画面の表示に成功すること' do
+      get events_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /events/new', :login do
+    it 'ログイン画面の表示に成功すること' do
+      get new_event_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /events/:id/edit', :login do
+    it 'ログイン画面の表示に成功すること' do
+      get edit_event_path(event.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /events/:id', :login do
+    it 'ログイン画面の表示に成功すること' do
+      get event_path(event.id)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
