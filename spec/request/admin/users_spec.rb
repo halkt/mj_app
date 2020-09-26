ea0a5290fddbd6f12a64f89a71f3c7b8ef9131e4
“halmuda” <halmuda6231@gmail.com>
2020-09-26 17:17:35 +0900

    update: #62 UsersControllerのset_authority_paramsのテスト追加

--- spec/request/admin/users_spec.rb
+++ spec/request/admin/users_spec.rb
@@ -27,20 +27,32 @@ RSpec.describe Admin::UsersController, type: :request do
         description: 'test',
         password: 'password',
         password_confirmation: 'password',
-        admin: true,
-        login_flg: true
+        authority: authority
       }
     end
     context '正常な値の場合' do
       let!(:user_name) { SecureRandom.alphanumeric(50) }
-      it '302が返ること' do
-        put admin_user_path(user.id), params: params
-        expect(response).to have_http_status(302)
+
+      context 'authorityがadminの場合' do
+        let!(:authority) { 'admin' }
+        it '302が返ること' do
+          put admin_user_path(user.id), params: params
+          expect(response).to have_http_status(302)
+        end
+      end
+
+      context 'authorityがloginの場合' do
+        let!(:authority) { 'login' }
+        it '302が返ること' do
+          put admin_user_path(user.id), params: params
+          expect(response).to have_http_status(302)
+        end
       end
     end
 
     context '異常な値の場合' do
       let!(:user_name) { SecureRandom.alphanumeric(51) }
+      let!(:authority) { 'other' }
       it '200が返ること' do
         put admin_user_path(user.id), params: params
         expect(response).to have_http_status(200)
