3ff5854fd2ae50ed9bf95ff7e73ed097b1a7db09
“halmuda” <halmuda6231@gmail.com>
2020-09-26 16:28:33 +0900

    add: #62 usersのeditのテスト追加

--- spec/request/admin/users.rb
+++ spec/request/admin/users.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :request do
+  let!(:user) do
+    FactoryBot.create(
+      :user,
+      admin: true,
+      login_flg: true)
+  end
+
+  describe 'GET /admin/users/:id/edit', :login do
+    it '200が返ること' do
+      get edit_admin_user_path(user.id)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
