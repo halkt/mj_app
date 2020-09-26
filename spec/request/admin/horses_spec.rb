38ada10c0e917577867a24213951a038963179af
“halmuda” <halmuda6231@gmail.com>
2020-09-26 17:27:04 +0900

    add: HorsesControllerの登録、更新時のテスト追加

--- spec/request/admin/horses_spec.rb
+++ spec/request/admin/horses_spec.rb
@@ -36,6 +36,67 @@ RSpec.describe Admin::HorsesController, type: :request do
     end
   end
 
+  describe 'POST /admin/horses', :login do
+    let(:params) do
+      {
+        horse: horse_params
+      }
+    end
+    let(:horse_params) do
+      {
+        name: 'horse_name',
+        point1: 10_000,
+        point2: point2
+      }
+    end
+    context '正常な値の場合' do
+      let!(:point2) { 5_000 }
+      it '302が返ること' do
+        post admin_horses_path, params: params
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context '異常な値の場合' do
+      let!(:point2) { 20_000 }
+      it '200が返ること' do
+        post admin_horses_path, params: params
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'PUT /admin/horses/:id', :login do
+    let!(:user) { FactoryBot.create(:user, admin: true) }
+    let(:params) do
+      {
+        horse: horse_params
+      }
+    end
+    let(:horse_params) do
+      {
+        name: 'horse_name',
+        point1: 10_000,
+        point2: point2
+      }
+    end
+    context '正常な値の場合' do
+      let!(:point2) { 5_000 }
+      it '302が返ること' do
+        put admin_horse_path(horse.id), params: params
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context '異常な値の場合' do
+      let!(:point2) { 20_000 }
+      it '200が返ること' do
+        put admin_horse_path(horse.id), params: params
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
   describe 'DELETE /admin/horses/:id', :login do
     it '302が返ること' do
       delete admin_horse_path(horse.id)
