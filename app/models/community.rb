a109ddd55ffb2baa9ff9d5eabfdf91b217e6b1ee
“halmuda” <halmuda6231@gmail.com>
2020-09-26 15:58:59 +0900

    update: #58 communityモデルのvalidateを追加

--- app/models/community.rb
+++ app/models/community.rb
@@ -5,6 +5,8 @@ class Community < ApplicationRecord
   has_many :users, through: :community_users
   has_many :events, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 50 }
+
   # 特定のユーザーが所属しているコミュニティ
   scope :affiliation_user, (lambda do |user_id|
     joins(:users).where(users: { id: user_id })
